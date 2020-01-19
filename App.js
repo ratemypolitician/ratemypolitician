3eaff5ce5d6f5879e7ab255b30065ce6b6565d18
alkyu92 <alkyu92@gmail.com>
2020-01-19 22:59:23 +0800

    unhide status bar

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <StatusBar hidden={false} />
+        <StatusBar hidden={false} barStyle={'dark-content'} />
         <AppContainer />
       </View>
     )
