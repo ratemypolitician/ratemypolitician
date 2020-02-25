0dbd75e5ae512eb9ff72961aaca1a42b0913ad3f
ratemypolitician <rate.politician.my@gmail.com>
2020-02-25 13:47:53 +0800

    change firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,14 +2,14 @@ import * as Firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2rxgV0zB6N31mnRY20orHb0m42aWuNbU",
-  authDomain: "phmeter-df00d.firebaseapp.com",
-  databaseURL: "https://phmeter-df00d.firebaseio.com",
-  projectId: "phmeter-df00d",
-  storageBucket: "phmeter-df00d.appspot.com",
-  messagingSenderId: "350459418918",
-  appId: "1:350459418918:web:d2d5eca29ff02e29b289e7",
-  measurementId: "G-FDZFFPWQVE"
+  apiKey: "AIzaSyD-75j8ryxh7ppmADKHBbHPi9rVGb4c3L4",
+  authDomain: "ratemypolitician-925cb.firebaseapp.com",
+  databaseURL: "https://ratemypolitician-925cb.firebaseio.com",
+  projectId: "ratemypolitician-925cb",
+  storageBucket: "ratemypolitician-925cb.appspot.com",
+  messagingSenderId: "937341828524",
+  appId: "1:937341828524:web:371590ecdf1f1acd340e09",
+  measurementId: "G-VX49G4N14N"
 };
 
 export const firebase = Firebase.initializeApp(firebaseConfig);
